ff6fb31135de31e8ed471bb0b7796194c0e97b11
JHpusd <justinhongdebate@gmail.com>
2021-02-01 23:55:41 +0000

    shortened plot method

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -21,33 +21,25 @@ class EulerEstimator():
         new_point = (t + step_size, new_x)
         return new_point
     
-    def calc_estimated_points(self, point, step_size, num_steps):
+    def calc_estimated_points(self, point, step_size, max_value):
         point_list = []
         point_list.append(point)
-        for i in range(num_steps):
+        counter = 0
+        while counter < max_value:
             next_point = self.step_forward(point, step_size)
             point_list.append(next_point)
             point = next_point
+            counter += step_size
         return point_list
 
     def plot(self, point, step_size, max_value):
-        t_list = []
-        list_dict = {}
-        for key in point[1]:
-            list_dict[key] = []
-        while True:
-            t = point[0]
-            if t > max_value:
-                break
-            t_list.append(t)
-            x = point[1]
-            for key in x:
-                list_dict[key].append(x[key])
-            point = self.step_forward(point, step_size)
         plt.style.use('bmh')
-        for key in list_dict:
-            plt.plot(t_list, list_dict[key], label=key)
+        points = self.calc_estimated_points(point, step_size, max_value)
+        t_list = [point[0] for point in points]
+        values = [point[1] for point in points]
+        for key in point[1]:
+            plt.plot(t_list, [vals[key] for vals in values], label=key)
         plt.legend(loc="upper left")
-        plt.savefig('plot.png')
+        plt.savefig("plot.png")
 
 
